b60cd19b358360df58d10edcb18a6ff2cf907ee0
Da Silva Junior <W0433181@campus.nscc.ca>
2019-09-24 09:21:55 -0300

    tech check 2

---
+++ Assignments/Assignment1/template.py
@@ -0,0 +1,23 @@
+###########################################
+# Desc: .
+#
+# Author: Laercio.
+###########################################
+
+def main():
+    # Main function for execution of program code.
+    # Make sure you tab once for every line.
+
+    # Input
+    name = input("Enter the customer's name: ")
+    deliveryDistance = input("Enter the distance in kilometers for delivery: ")
+    recordsCost = input("Enter the cost of records purchased: ")
+
+    # Process
+
+    # Output
+    print("Hello World")
+
+#PROGRAM STARTS HERE. DO NOT CHANGE THIS CODE.
+if __name__ == "__main__":
+    main()
---
+++ TechChecks/TC2/TechCheck2.py
@@ -0,0 +1,41 @@
+###########################################
+# Desc: .
+#
+# Author: Laercio.
+###########################################
+
+def main():
+    # Main function for execution of program code.
+    # Make sure you tab once for every line.
+
+    # Input
+    # weeklySalary = 1000
+    weeklySalary = input("Please enter the full amount of your weekly salary: ")
+    # dependents = 2
+    dependents = input("How many dependents do you have? ")
+    provincialTaxRate = 0.06
+    federalTaxRate = 0.25
+    dependentsTaxRate = 0.02
+
+    # Process
+    provincialTaxWithheld = float(weeklySalary) * float(provincialTaxRate)
+    federalTaxWithheld = float(weeklySalary) * float(federalTaxRate)
+    dependentDeduction = float(weeklySalary) * (int(dependents) * float(dependentsTaxRate))
+    totalWithheld = (provincialTaxWithheld + federalTaxWithheld) - dependentDeduction
+    totalTakeHomePay = float(weeklySalary) - totalWithheld
+
+    # Output
+    #print("{0:.2f}".format(provincialTaxWithheld))
+    print("Provincial Tax Withheld: ${0:.2f}".format(provincialTaxWithheld))
+    #print("{0:.2f}".format(federalTaxWithheld))
+    print("Federal Tax Withheld: ${0:.2f}".format(federalTaxWithheld))
+    #print("{0:.0f} {1:.2f}".format(int(dependents), dependentDeduction))
+    print("Dependent Deduction for {0:.0f} dependents: ${1:.2f}.".format(int(dependents), dependentDeduction))
+    #print("{0:.2f}".format(totalWithheld))
+    print("Total Withheld: ${0:.2f}".format(totalWithheld))
+    #print("{0:.2f}".format(totalTakeHomePay))
+    print("Total Take-Home Pay: ${0:.2f}".format(totalTakeHomePay))
+
+#PROGRAM STARTS HERE. DO NOT CHANGE THIS CODE.
+if __name__ == "__main__":
+    main()
