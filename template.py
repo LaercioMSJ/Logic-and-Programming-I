d040155f95fa6074445cae7e602b4b7a7525d5c3
Da Silva Junior <W0433181@campus.nscc.ca>
2019-11-05 09:33:28 -0400

    matrix 2D examples

--- template.py
+++ template.py
@@ -6,16 +6,31 @@
 # Author: Laercio M da Silva Junior - W0433181
 ###########################################
 
+import random
+
 def main():
     # Main function for execution of program code.
     # Make sure you tab once for every line.
 
     # INPUT
+    rows = 5
+    cols = 6
+    
+    matrix = [ ([0] * cols) for row in range(rows) ]
 
     # PROCESS
+    for i in range(len(matrix)):
+        for x in range (len(matrix[i])):
+            matrix[i][x] = random.randint(1, 100)
 
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print(matrix[row][col] , end = "\t")
+        print()
+
+    
     # OUTPUT
-    print("Hello World")
 
 #PROGRAM STARTS HERE. DO NOT CHANGE THIS CODE.
 if __name__ == "__main__":
